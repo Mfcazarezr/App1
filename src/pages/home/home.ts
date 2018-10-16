822dd42d282d60de09169f6540cff3ebfafaed86
alumno Secundaria <al3201@cedi.local>
2018-10-16 09:07:06 -0500

    quiz

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,44 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LamparaPage } from '../lampara/lampara';
+import { BocinaPage } from '../bocina/bocina';
+import { TvPage } from '../tv/tv';
+import { SmartphonePage } from '../smartphone/smartphone';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  lamparas=LamparaPage;
+  bocinas= BocinaPage;
+  tvs = TvPage;
+  smartphones =SmartphonePage;
 
   constructor(public navCtrl: NavController) {
 
   }
+  lampara()
+  {
+    this.navCtrl.push (this.lamparas);
+  }
 
-}
+  bocina()
+  {
+    this.navCtrl.push (this.bocinas);
+  }
+
+  
+  tv()
+  {
+    this.navCtrl.push (this.tvs);
+  }
+  
+  smartphone()
+  {
+    this.navCtrl.push (this.smartphones);
+  }
+
+  }
+
+
